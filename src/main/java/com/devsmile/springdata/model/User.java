be2201df9ee5ae42ed29b56d06dbafd52511ba9e
Virus_Mix <sashamoik2016@gmail.com>
2019-07-30 20:29:13 +0300

    Addet to User class methods - hashCode and equals.

--- src/main/java/com/devsmile/springdata/model/User.java
+++ src/main/java/com/devsmile/springdata/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "user")
 public class User {
-    
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name = "id")
@@ -22,24 +22,20 @@ public class User {
     @Column(name = "age")
     private Integer  age;
 
-    public User() {
+    public User(Integer id, String firstName, String lastName, Integer age) {
         super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
 
-    public User(Integer id, String firstName,String lastName, Integer age) {
-        super();
-        this.id=id;
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.age=age;
-    }
-    
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
-        this.id=id;
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -63,11 +59,54 @@ public class User {
     }
 
     public void setAge(Integer age) {
-        this.age=age;
+        this.age = age;
     }
 
     @Override
     public String toString() {
         return String.format("User [id=%d, firstName=%s, lastName=%s, age=%d]", id,firstName,lastName,age);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((age == null) ? 0 : age.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (age == null) {
+            if (other.age != null)
+                return false;
+        } else if (!age.equals(other.age))
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        return true;
     }
 }
