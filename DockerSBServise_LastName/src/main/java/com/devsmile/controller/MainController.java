3073582eb89b7832e3dae10c91587cae1a14630d
Virus_Mix <sashamoik2016@gmail.com>
2019-09-27 18:13:35 +0300

    simple pattern

---
+++ DockerSBServise_LastName/src/main/java/com/devsmile/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.devsmile.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    
+    @GetMapping
+    public String mainPage() {
+        return "service last name";
+    }
+}
