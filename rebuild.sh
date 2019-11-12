e9038ab43738c7f018225009546ebf07b8c80d90
Virus_Mix <sashamoik2016@gmail.com>
2019-11-12 10:41:18 +0200

    test

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
-cd ./DockerSBServise_Age/ && gradle clean build && docker build . -t service-age:v1 &
-cd ./DockerSBServise_FirstName/ && gradle clean build && docker build . -t service-firsname:v1 &
-cd ./DockerSBServise_LastName/ && gradle clean build && docker build . -t service-lastname:v1
+cd ./DockerSBServise1_Age/ && gradle clean build && docker build . -t service-1-age:v1 &
+cd ./DockerSBServise3_FirstName/ && gradle clean build && docker build . -t service-3-firsname:v1 &
+cd ./DockerSBServise2_LastName/ && gradle clean build && docker build . -t service-2-lastname:v1
